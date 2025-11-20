47505c4df7e412f99b582daa9f5b8f1ace77902d
KarinaGheorghe <gheorghekarina22@stud.ase.ro>
2025-11-20 23:17:35 +0200

    tema3

--- activitate_POO.cpp
+++ activitate_POO.cpp
@@ -257,11 +257,121 @@ public:
 double Medie3(const Echipament2& a, const Echipament2& b, const Echipament2& c) {
 	return (*a.greutate + *b.greutate + *c.greutate) / 3;
 }
+//Tema3
+class Pacient3 {
+private:
+	string nume;
+	int varsta;
+	double greutate;
 
+public:
+	Pacient3() : nume("Anonim"), varsta(30), greutate(70) {}
+	Pacient3(string n, int v, double g) : nume(n), varsta(v), greutate(g) {}
+
+	Pacient3& operator=(const Pacient3& p) {
+		if (this != &p) {
+			nume = p.nume;
+			varsta = p.varsta;
+			greutate = p.greutate;
+		}
+		return *this;
+	}
+
+	Pacient3 operator+(double kg) const { return Pacient3(nume, varsta, greutate + kg); }
+	Pacient3 operator-(int ani) const { return Pacient3(nume, varsta - ani, greutate); }
+	bool operator>(const Pacient3& p) const { return varsta > p.varsta; }
+
+	friend void FriendP(const Pacient3& p);
+
+	void Afisare() const {
+		cout << "P3: " << nume << ", " << varsta << ", " << greutate << "kg\n";
+	}
+};
+
+void FriendP(const Pacient3& p) {
+	cout << "[FRIEND] Pacient3 " << p.nume << " are " << p.varsta << " ani.\n";
+}
+
+class Exercitiu3 {
+private:
+	string denumire;
+	int repetari;
+	double durata;
+
+public:
+	Exercitiu3() : denumire("Genuflexiuni"), repetari(10), durata(2) {}
+	Exercitiu3(string d, int r, double du) : denumire(d), repetari(r), durata(du) {}
+
+	Exercitiu3& operator=(const Exercitiu3& e) {
+		if (this != &e)
+		{
+			denumire = e.denumire;
+			repetari = e.repetari;
+			durata = e.durata;
+		}
+		return *this;
+	}
+
+	Exercitiu3 operator+(int r) const { return Exercitiu3(denumire, repetari + r, durata); }
+	bool operator<(const Exercitiu3& e) const { return durata < e.durata; }
+	bool operator==(const Exercitiu3& e) const { return denumire == e.denumire; }
+
+	friend void FriendE(const Exercitiu3& e);
+
+	void Afisare() const
+	{
+		cout << "E3: " << denumire << ", rep: " << repetari
+			<< ", durata: " << durata << endl;
+	}
+};
+
+void FriendE(const Exercitiu3& e)
+{
+	cout << "[FRIEND] Ex3: " << e.denumire << " are " << e.repetari << " rep.\n";
+}
+
+class Echipament3 
+{
+private:
+	string tip;
+	double greutate;
+	bool functional;
+
+public:
+	Echipament3() : tip("Bicicleta"), greutate(25), functional(true) {}
+	Echipament3(string t, double g, bool f) : tip(t), greutate(g), functional(f) {}
+
+	Echipament3& operator=(const Echipament3& e)
+	{
+		if (this != &e) {
+			tip = e.tip;
+			greutate = e.greutate;
+			functional = e.functional;
+		}
+		return *this;
+	}
+
+	Echipament3 operator+(double g) const { return Echipament3(tip, greutate + g, functional); }
+	bool operator>(const Echipament3& e) const { return greutate > e.greutate; }
+	bool operator!() const { return !functional; }
+
+	friend void FriendEq(const Echipament3& e);
+
+	void Afisare() const
+	{
+		cout << "Eq3: " << tip << ", " << greutate << "kg, func: "
+			<< (functional ? "DA" : "NU") << endl;
+	}
+};
+
+void FriendEq(const Echipament3& e)
+{
+	cout << "[FRIEND] Eq3: " << e.tip << " cantareste " << e.greutate << " kg.\n";
+}
 int main() {
 	cout << "=== Domeniu: Geokinetica (Gheorghe Karina) ===\n\n";
 
-		Pacient p1;
+	Pacient p1;
 	Pacient p2("Ion Popescu", 65);
 	Pacient p3("Maria Georgescu", 40, 58.5, true, "adult");
 	p1.Afisare(); p2.Afisare(); p3.Afisare();
@@ -296,5 +406,36 @@ int main() {
 	Echipament2 eq23(eq22);
 	cout << "Medie = " << Medie3(eq21, eq22, eq23) << endl;
 
-		return 0;
+	Pacient3 t31("Karina", 22, 55);
+	Pacient3 t32("Ion", 30, 75);
+	Pacient3 t33;
+
+	t33 = t31;
+	t33 = t33 + 5;
+	t33 = t33 - 2;
+
+	t31.Afisare(); t32.Afisare(); t33.Afisare();
+	FriendP(t31);
+
+	Exercitiu3 te31("Flotari", 15, 2);
+	Exercitiu3 te32("Flotari", 10, 3);
+	Exercitiu3 te33;
+
+	te33 = te31;
+	te33 = te33 + 5;
+
+	te31.Afisare(); te32.Afisare(); te33.Afisare();
+	FriendE(te31);
+
+	Echipament3 t41("Banda", 50, true);
+	Echipament3 t42("Minge", 2, false);
+	Echipament3 t43;
+
+	t43 = t41;
+	t43 = t43 + 3;
+
+	t41.Afisare(); t42.Afisare(); t43.Afisare();
+	FriendEq(t41);
+
+	return 0;
 }
